81b23ace54ad7572259d6fbb232472e78cde7329
Jose Luis <joseluisr@twitter.com>
2019-12-03 10:01:59 -0800

    day3

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -1,4 +1,5 @@
-use std::ops::Add;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 #[derive(Debug)]
 enum Directions {
@@ -13,8 +14,8 @@ struct Step {
     num: u32,
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Point(u32, u32);
+#[derive(Debug, PartialEq, Hash, Eq)]
+struct Point(i32, i32);
 impl Point {
     fn displace(&self, other: Point) -> Point {
         Point(self.0 + other.0, self.1 + other.1)
@@ -33,22 +34,48 @@ impl From<&str> for Step {
         Step { dir, num }
     }
 }
+
 fn get_vector(steps: Vec<Step>) -> Vec<Point> {
-    let mut start = vec![Point(0, 0)];
+    let mut v = vec![Point(0, 0)];
     steps.iter().for_each(|x| match x {
         Step {
             dir: Directions::D,
             num,
-        } => start.push(start.last().unwrap().displace(Point(0, 1))),
-        _ => unreachable!(),
+        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(0, -1))),
+        Step {
+            dir: Directions::U,
+            num,
+        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(0, 1))),
+        Step {
+            dir: Directions::R,
+            num,
+        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(1, 0))),
+        Step {
+            dir: Directions::L,
+            num,
+        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(-1, 0))),
     });
-    start
+    v
 }
+fn manhattan(a: &Point) -> i32 {
+    a.0.abs() + a.1.abs()
+}
+fn push_into_vec(v: &mut Vec<Point>, point: Point) {
+    v.push(v.last().unwrap().displace(point));
+}
+
 fn main() {
-    let res: Vec<Vec<Point>> = include_str!("day_03_data.txt")
+    let points = include_str!("day_03_data.txt")
         .lines()
         .map(|x| x.split(',').map(|x| Step::from(x)).collect())
         .map(get_vector)
-        .collect();
-    dbg!(res);
+        .map(HashSet::from_iter)
+        .collect::<Vec<HashSet<Point>>>();
+
+    points[0]
+        .intersection(&(points[1]))
+        .map(manhattan)
+        .filter(|x| *x != 0)
+        .min()
+        .map(|x| dbg!(x));
 }
