1a808a31331eab54bfefaa903e8dbefb3b33b865
Jose Luis <joseluisr@twitter.com>
2019-12-03 11:16:44 -0800

    improvements

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -17,17 +17,18 @@ struct Step {
 #[derive(Debug, PartialEq, Hash, Eq)]
 struct Point(i32, i32);
 impl Point {
-    fn displace(&self, other: Point) -> Point {
+    fn displace(&self, other: &Point) -> Point {
         Point(self.0 + other.0, self.1 + other.1)
     }
 }
 impl From<&str> for Step {
     fn from(item: &str) -> Self {
+        use crate::Directions::*;
         let dir = match item.chars().nth(0).unwrap() {
-            'D' => Directions::D,
-            'U' => Directions::U,
-            'R' => Directions::R,
-            'L' => Directions::L,
+            'D' => D,
+            'U' => U,
+            'R' => R,
+            'L' => L,
             _ => unreachable!(),
         };
         let num = item[1..].parse().unwrap();
@@ -41,33 +42,38 @@ fn get_vector(steps: Vec<Step>) -> Vec<Point> {
         Step {
             dir: Directions::D,
             num,
-        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(0, -1))),
+        } => push_into_vec_n(&mut v, *num, Point(0, -1)),
         Step {
             dir: Directions::U,
             num,
-        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(0, 1))),
+        } => push_into_vec_n(&mut v, *num, Point(0, 1)),
         Step {
             dir: Directions::R,
             num,
-        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(1, 0))),
+        } => push_into_vec_n(&mut v, *num, Point(1, 0)),
         Step {
             dir: Directions::L,
             num,
-        } => (0..*num).for_each(|_| push_into_vec(&mut v, Point(-1, 0))),
+        } => push_into_vec_n(&mut v, *num, Point(-1, 0)),
     });
     v
 }
+
+fn push_into_vec_n(mut v: &mut Vec<Point>, num: u32, point: Point) -> () {
+    (0..num).for_each(|_| push_into_vec(&mut v, &point))
+}
+
 fn manhattan(a: &Point) -> i32 {
     a.0.abs() + a.1.abs()
 }
-fn push_into_vec(v: &mut Vec<Point>, point: Point) {
-    v.push(v.last().unwrap().displace(point));
+fn push_into_vec(v: &mut Vec<Point>, point: &Point) {
+    v.push(v.last().unwrap().displace(&point));
 }
 
 fn main() {
     let points = include_str!("day_03_data.txt")
         .lines()
-        .map(|x| x.split(',').map(|x| Step::from(x)).collect())
+        .map(|x| x.split(',').map(|x| x.into()).collect())
         .map(get_vector)
         .map(HashSet::from_iter)
         .collect::<Vec<HashSet<Point>>>();
