56edc6b3d262ea1728a41eb2808845c3ee7f43bb
jlricon <artirj@gmail.com>
2019-12-18 08:26:10 +0000

    iterator for day 16

--- src/bin/day_16.rs
+++ src/bin/day_16.rs
@@ -1,30 +1,34 @@
+use itertools::Itertools;
+
 const BASE_PATTERN: [i32; 4] = [0, 1, 0, -1];
 fn input_list_times_pattern<'a>(x: impl Iterator<Item = &'a u32>, pos: usize) -> u32 {
     let pattern = BASE_PATTERN
         .iter()
-        .skip({
-            if pos == 1 {
-                1
-            } else {
-                0
-            }
-        })
-        .cycle();
+        .chain(BASE_PATTERN.iter())
+        .flat_map(|v| std::iter::repeat(v).take(pos))
+        .cycle()
+        .skip(1);
 
     x.zip(pattern)
-        .map(|(l, p)| {
-            dbg!(l, p);
-            *l as i32 * p
-        })
+        .map(|(l, p)| *l as i32 * p)
         .sum::<i32>()
         .abs() as u32
         % 10
+}
+fn apply_1_phase(inp: &Vec<u32>, n: usize) -> Vec<u32> {
+    (1..=inp.len())
+        .map(|n| input_list_times_pattern(inp.iter(), n))
+        .collect()
+}
+fn apply_n_phases<'a>(x: impl Iterator<Item = &'a u32>, n: usize) -> u32 {
+    1
 }
 fn main() {
     //    let inp = include_str!("day_16_data.txt");
 
     let list = input_list_times_pattern([0, 1, 2].iter(), 1);
 }
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -39,5 +43,11 @@ mod test {
         assert_eq!(o1, 4);
         let o1 = input_list_times_pattern(inp.iter(), 2);
         assert_eq!(o1, 8);
+        let o1 = input_list_times_pattern(inp.iter(), 3);
+        assert_eq!(o1, 2);
+        let full: Vec<_> = (1..=inp.len())
+            .map(|n| input_list_times_pattern(inp.iter(), n))
+            .collect();
+        assert_eq!(full, vec![4, 8, 2, 2, 6, 1, 5, 8])
     }
 }
